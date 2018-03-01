a7eef1191237ceac0d9633cba45627964181aa89
jaymeans <jaymeans313@gmail.com>
2018-02-28 21:56:17 -0800

    initial

---
+++ src/file.ts
@@ -0,0 +1,27 @@
+export default class File {
+  public static rawData: string
+  // csv
+  // jsonRows
+  // filename
+  // isManaged
+  // countRows
+  // skippedRows
+  // file
+  // getBatchID
+  // columns
+  // countColumns
+  // columnsMatched
+  // isManual
+  // countRowsInvalid
+  // headersRaw
+  // countColumnsMatched
+  // importedFromURL
+  // failureReason
+  // submittedAt
+  // failedAt
+  // createdAt
+  // handledAt
+  // validatedIn
+  // matchedIn
+  // config
+}
