70aaabb43dc400ae7bd80727cf24612712d4a3cd
David Boskovic <me@david.gs>
2018-04-23 21:12:58 -0600

    some minor tweaks

--- src/upload-file.ts
+++ src/upload-file.ts
@@ -7,22 +7,37 @@ export default class UploadFile {
     this.$file = file
   }
 
+  /**
+   * A unique UUID referencing this file in the Flatfile system
+   */
   get id () {
     return this.$file.id
   }
 
+  /**
+   * The original filename on the user's system
+   */
   get filename () {
     return this.$file.filename
   }
 
+  /**
+   * The size of the file in bytes
+   */
   get filesize () {
     return this.$file.filesize
   }
 
+  /**
+   * The type of file
+   */
   get filetype () {
     return this.$file.filetype
   }
 
+  /**
+   * A securely signed url giving you temporary access to download the file
+   */
   get url () {
     return this.$file.url
   }
