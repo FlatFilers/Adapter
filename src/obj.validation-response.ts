13b3ebccb7d2bb4988e80d19ff822ac89b6add16
Max Blank <blank.maksym@gmail.com>
2021-01-20 13:00:03 -0600

    refactor(adapter): fixed validation response

--- src/obj.validation-response.ts
+++ src/obj.validation-response.ts
@@ -1,3 +1,20 @@
+export default interface IValidationResponse {
+  /**
+   * A string referencing the key affected by the problem
+   */
+  key: string
+
+  /**
+   * The validation error message
+   */
+  message: string
+
+  /**
+   * The type of validation response - currently only 'error' is supported
+   */
+  level?: 'error'
+}
+
 export interface IDataHookResponse {
   [key: string]: IDataHookRecord
 }
